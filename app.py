258c92c59483a337acbcaebd5fdb3b2d9eba5f5e
Marek Cech <cechsmarek@gmail.com>
2024-09-29 19:33:00 +0200

    fixed duplicated creation of database in app.py, removed unnecessary functions from database.py (dabase is now created directly in app.py) and changed if statement from checking for chroma directory to checking if vector dabase contains embeddings

--- app.py
+++ app.py
@@ -5,14 +5,13 @@ from langchain_community.vectorstores import Chroma
 #from langchain_community.llms import Ollama
 import os
 from doc_loader import LoadDocuments, RemoveDuplicates
-from database import CreateVectorDB, AddToVectorDB
 from llm import GenerateResponse
 
 promptTemplate = """Context information is below.
 ---------------------
 [context_str]
 ---------------------
-Given the context information and not prior knowledge, answer the query.
+Given the context information and not prior knowledge, answer the query. Context may contain duplicates.
 Query: [query_str]
 Answer:"""
 
@@ -20,20 +19,21 @@ model_name = "llama3.1"
 db_directory = "./chroma"
 doc_directory = "./documents"
 
-if not os.path.exists(db_directory):
+embeddings = OllamaEmbeddings(model=model_name)
+vectordb = Chroma(persist_directory=db_directory, embedding_function=embeddings)
+embeddings_count = vectordb._collection.count()
+
+if embeddings_count == 0:
     print("Vector database does not exist. Creating a new one.")
     texts = LoadDocuments(doc_directory)
-    vectordb, embeddings = CreateVectorDB(texts, model_name=model_name, persist_directory=db_directory)
-    vectordb = AddToVectorDB(vectordb, texts, embeddings)
+    vectordb.add_texts(texts=texts, embeddings=embeddings)
 
 else:
     print("Vector database exists. Loading the existing database.")
-    embeddings = OllamaEmbeddings(model=model_name)
-    vectordb = Chroma(persist_directory=db_directory, embedding_function=embeddings)
 
 query = "Where is James Webb Telescope now?"
 retrieved_docs = vectordb.similarity_search(query, k=3)
-unique_docs = RemoveDuplicates(retrieved_docs)
+unique_docs = retrieved_docs
 
 response = GenerateResponse(unique_docs, query, model_name, promptTemplate)
 
